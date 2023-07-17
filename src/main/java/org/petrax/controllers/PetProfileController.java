a99df7216555d79e6e94e44561be4bcfb8a59d8e
tayleredelen <taylerledelen@gmail.com>
2023-07-17 17:51:15 -0500

    Tayler--edited build.gradle file to implement thymeleaf html template, successfully got an Add New Pet button to appear with pop-up!

--- src/main/java/org/petrax/controllers/PetProfileController.java
+++ src/main/java/org/petrax/controllers/PetProfileController.java
@@ -1,46 +1,52 @@
 package org.petrax.controllers;
+
 import org.petrax.data.PetProfileRepository;
-import org.petrax.data.PetTypeRepository;
 import org.petrax.models.PetProfile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
-import java.util.Optional;
 
 @Controller
 @RequestMapping("petProfile")
 public class PetProfileController {
 
-    //Refresher: @Autowired annotation specifies that SB should auto-populate this field.
-    //This is a dependency injection, what happens is the Autowired code tells SB we need a PetProfileRepository object
     @Autowired
     private PetProfileRepository petProfileRepository;
-
-    @Autowired
-    private PetTypeRepository petTypeRepository;
 
     @GetMapping("addnewpet")
     public String displayAddNewPetForm(Model model) {
         model.addAttribute("add", "Add New Pet");
-        model.addAttribute(new PetProfile());
-        model.addAttribute("petProfile", petProfileRepository.findAll());
+        model.addAttribute("petProfile", new PetProfile());
         return "petProfile/addnewpet";
     }
 
     @PostMapping("addnewpet")
-    public String processAddNewPetForm(@ModelAttribute @Valid PetProfile newPet,
-                                         Errors errors, Model model) {
-        if(errors.hasErrors()) {
+    public String processAddNewPetForm(@ModelAttribute("petProfile") @Valid PetProfile newPet,
+                                       Errors errors, Model model) {
+        if (errors.hasErrors()) {
             model.addAttribute("add", "Add New Pet");
             return "petProfile/addnewpet";
         }
+
         petProfileRepository.save(newPet);
-        return "redirect:";
+        return "redirect:/petProfile/success";
     }
+
+    @GetMapping("success")
+    public String displaySuccessPage() {
+        return "success";
+    }
+
+
+
+
 //
 //    @GetMapping("deletepet")
 //    public String displayDeletePetForm(Model model) {
--- src/main/java/org/petrax/data/PetTypeRepository.java
+++ src/main/java/org/petrax/data/PetTypeRepository.java
@@ -1,14 +1,14 @@
-package org.petrax.data;
-import org.petrax.models.PetType;
-import org.springframework.stereotype.Repository;
-import org.springframework.data.repository.CrudRepository;
+//package org.petrax.data;
+//import org.petrax.models.PetType;
+//import org.springframework.stereotype.Repository;
+//import org.springframework.data.repository.CrudRepository;
+//
+//
+////Refresher: the @Repository annotation tells SB it's a class we want available in other parts of app
+////A repository in Java is a part of the code that handles data storage and retrieval
+////Extends CrudRepository so we can perform CRUD operations on data
+//@Repository
+//public interface PetTypeRepository extends CrudRepository<PetType, Integer> {
+//}
 
-
-//Refresher: the @Repository annotation tells SB it's a class we want available in other parts of app
-//A repository in Java is a part of the code that handles data storage and retrieval
-//Extends CrudRepository so we can perform CRUD operations on data
-@Repository
-public interface PetTypeRepository extends CrudRepository<PetType, Integer> {
-}
-
-
+//not allowing me to bootrun?
