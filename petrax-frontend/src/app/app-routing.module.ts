045cae6a8a1a3fd46a65b1fca30d6f1f59d8b7cb
tayleredelen <taylerledelen@gmail.com>
2023-07-31 18:05:15 -0500

    Tayler--backend forms work with Ryan's date format fix

--- petrax-frontend/src/app/app-routing.module.ts
+++ petrax-frontend/src/app/app-routing.module.ts
@@ -6,10 +6,6 @@ import { UserListComponent } from './user-list/user-list.component';
 import { CalendarComponent } from './calendar/calendar.component';
 import { HomeComponent } from './home/home.component';
 import { FaqComponent } from './faq/faq.component';
-import { AddPetComponent } from './add-pet/add-pet.component';
-import { AddPetSuccessComponent } from './add-pet/add-pet-success.component';
-
-
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' }, // Default route redirects to home
@@ -17,9 +13,6 @@ const routes: Routes = [
   { path: 'calendar', component: CalendarComponent }, // Route for CalendarComponent
   { path: 'users', component: UserListComponent }, // Route for CalendarComponent
   {path: 'faq', component: FaqComponent}, //Route for FAQ
-  {path: 'add-pet', component: AddPetComponent}, //Route for AddPetComponent
-  {path: 'add-pet-success', component: AddPetSuccessComponent}, //Route for nested AddPetSuccessComponent
-
 
   // Add more routes as needed
 ];
