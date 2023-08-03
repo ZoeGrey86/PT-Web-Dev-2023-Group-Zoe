d1854f832d601dae8f8dcf6feda62b2b02ccd12e
tayleredelen <taylerledelen@gmail.com>
2023-08-03 17:30:10 -0500

    Tayler--rebuilt pet-profile component and updated birthdate data type on add pet and delete pet

--- src/main/java/org/petrax/data/PetProfileRepository.java
+++ src/main/java/org/petrax/data/PetProfileRepository.java
@@ -2,7 +2,6 @@ package org.petrax.data;
 import org.petrax.models.PetProfile;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
 import java.time.LocalDate;
 
 
