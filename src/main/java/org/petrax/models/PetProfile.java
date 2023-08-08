06831f740fed885ff256068d8a1e43e413157d55
tayleredelen <taylerledelen@gmail.com>
2023-08-07 20:06:12 -0500

    Tayler -- updated PetProfile

--- src/main/java/org/petrax/models/PetProfile.java
+++ src/main/java/org/petrax/models/PetProfile.java
@@ -2,109 +2,37 @@ package org.petrax.models;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import java.time.LocalDate;
-import java.util.Objects;
 
 @Entity
-//@Validated
 public class PetProfile {
 
     //private fields petId, name, breed, age, weight, birthday, medications, allergies, microchip number, and diagnoses
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int petId;
 
-    //The field to store the uploaded image as a byte array (BLOB)
-    private byte[] petPic;
-
-    //    @NotBlank(message = "Name is required")
-    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
     private String name;
-
-    //Enum class PetType
-    @NotNull(message = "Must select pet type")
     private PetType petType;
-
-    @NotBlank(message = "Breed is required")
-    @Size(min = 3, max = 50, message = "Breed must be between 3 and 50 characters")
     private String breed;
-
-    @NotNull(message = "Age is required")
     private Double age;
-
-    @NotNull(message = "Weight is required")
     private Double weight;
-
-    @NotNull(message = "Birthdate is required")
-
     private LocalDate birthdate;
-
-    @NotBlank(message = "Enter 'None' if not applicable")
-    @Size(max = 500, message = "Medications too long!")
     private String medication;
-
-    @NotBlank(message = "Enter 'None' if not applicable")
-    @Size(max = 500, message = "Allergies too long!")
     private String allergy;
-
-    @NotBlank(message = "Enter 'None' if not applicable")
-    @Size(max = 500, message = "Microchip number too long!")
-    //Some microchip #s are alpha-numeric, so string not double
     private String microchip;
-
-    @NotBlank(message = "Enter 'None' if not applicable")
-    @Size(max = 500, message = "Diagnoses too long!")
     private String diagnoses;
 
 
-    //public empty constructor, what the JPA uses to instantiate an object (entity class)
-    public PetProfile() {}
-
-
-    //public constructor used to instantiate an object
-    //Refresher: petId value not added since it will be auto-generated
-    //automatically when the object is persisted or saved.
-    public PetProfile(String name, byte[] petPic, PetType petType, String breed, double age, double weight, LocalDate birthdate,
-                      String medication, String allergy, String microchip, String diagnoses) {
-        this.name = name;
-        this.petPic = petPic; //blob SQL type
-        this.petType = petType; //enum declaration
-        this.breed = breed;
-        this.age = age;
-        this.weight = weight;
-        this.birthdate = birthdate;
-        this.medication = medication;
-        this.allergy = allergy;
-        this.microchip = microchip;
-        this.diagnoses = diagnoses;
-    }
-
-
-
-    //public getters & setters
-    //Refresher: Setter methods are used to set the value of a field/property in an object
-    //If a field is not meant to be changed once it is set, omit the setter
-    //No setter for name, breed, birthdate, microchip, or petId (petId will be auto-generated)
-    //Added setters to all, trying to resolve sql connection issue
     public int getPetId() {
         return petId;
     }
 
     public void setPetId(int petId) {
         this.petId = petId;
-    }
-
-    public byte[] getPetPic() {
-        return petPic;
-    }
-
-    public void setPetPic(byte[] petPic) {
-        this.petPic = petPic;
     }
 
     public String getName() {
@@ -185,44 +113,6 @@ public class PetProfile {
 
     public void setDiagnoses(String diagnoses) {
         this.diagnoses = diagnoses;
-    }
-
-
-    //public toString method of most relevant/identifying field petId
-
-    //Refresher: Typically, you would include the fields that are most relevant or important
-    //for understanding the state of the object. This can include fields that provide
-    //key information or help identify the object. It's generally a good practice to
-    //include a combination of important fields rather than blindly including all fields.
-    @Override
-    public String toString() {
-        return "PetProfile{" +
-                "petId=" + petId +
-                '}';
-    }
-
-    //public equals method checks if two PetProfile objects are equal based on the petId field only
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        PetProfile that = (PetProfile) o;
-        return petId == that.petId;
-    }
-
-
-    //public hashCode method computes and returns an integer value that represents the object's hash code,
-    // typically used for efficient storage and retrieval in hash-based data structures.
-
-    //Refresher: The hashCode method is used in Java to generate a hash code for an object.
-    //The hash code is an integer value that is typically used for efficient storage and
-    //retrieval of objects in hash-based data structures like HashMap, HashSet, and others.
-    //In the hashCode method you provided, the hash code is computed based on the petId field
-    //using the Objects.hash method. By overriding the hashCode method, you ensure that
-    //objects of the PetProfile class can be used properly in hash-based data structures.
-    @Override
-    public int hashCode() {
-        return Objects.hash(petId);
     }
 
 
